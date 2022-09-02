227d48a15db6f30f2c5a3a94b8c39cde099a12e9
ferruh <ferruhcihan20@gmail.com>
2022-09-03 01:36:39 +0300

    add background images

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,59 @@
-import React from 'react';
-import {ImageBackground, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {ImageBackground, Text, StyleSheet} from 'react-native';
+import orange from '../assets/orange_base64.json';
 
 const App = () => {
-  const bgImg = require('../assets/youtube.png');
+  const [index, setIndex] = useState(0);
+  const images = [
+    {
+      name: 'Pineapple',
+      src: require('../assets/pineapple.jpg'),
+    },
+    {
+      name: 'Orange',
+      src: {
+        uri: orange.base64,
+      },
+    },
+    {
+      name: 'Raspberry',
+      src: {
+        uri: 'https://images.unsplash.com/photo-1615484476889-2830f980a5e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      },
+    },
+  ];
 
-  return <ImageBackground source={bgImg} style={styles.container} />;
+  useEffect(() => {
+    setTimeout(() => {
+      index === 2 ? setIndex(0) : setIndex(prev => prev + 1);
+    }, 3000);
+  }, [index]);
+
+  return (
+    <ImageBackground
+      source={images[index].src}
+      resizeMode="cover"
+      style={styles.image}>
+      <Text style={styles.text}>{images[index].name}</Text>
+    </ImageBackground>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#000000c0',
   },
 });
 
